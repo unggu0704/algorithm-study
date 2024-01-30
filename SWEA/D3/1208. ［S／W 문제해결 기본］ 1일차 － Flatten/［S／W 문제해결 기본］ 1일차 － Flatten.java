f4001fd8d79f8c3a743fd8163621ee3e7b422ba3
Unggu <130115689+unggu0704@users.noreply.github.com>
2024-01-30 14:10:42 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 130 ms, Memory: 21,196 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,80 +1,58 @@
-import java.io.*;
-import java.util.*;
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////////////
-//기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-//아래 표준 입출력 예제 필요시 참고하세요.
-//표준 입력 예제
-//int a;
-//double b;
-//char g;
-//String var;
-//long AB;
-//a = sc.nextInt();                           // int 변수 1개 입력받는 예제
-//b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
-//g = sc.nextByte();                          // char 변수 1개 입력받는 예제
-//var = sc.next();                            // 문자열 1개 입력받는 예제
-//AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-//표준 출력 예제
-//int a = 0;                            
-//double b = 1.0;               
-//char g = 'b';
-//String var = "ABCDEFG";
-//long AB = 12345678901234567L;
-//System.out.println(a);                       // int 변수 1개 출력하는 예제
-//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
-//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
-//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
-//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
-/////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-/*
-사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
-*/
-public class Solution {
-	
-	static int N;
-	public static void main(String args[]) throws Exception
-		{
+/**
+ * 데일리 실습
+ * 
+ * @author 김규형
+ *
+ */
+public class Solution  {
+
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int T = 10;
-		//T=sc.nextInt();
-
-		/*
-		여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-		*/
-		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		for(int test_case = 1; test_case <= T; test_case++) {
-	        StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken()); // input count;
-			
-			Integer[] container = new Integer[100];
-			
-			st = new StringTokenizer(br.readLine());
-			for (int i = 0; i < 100; i++) {
-				container[i] = Integer.parseInt(st.nextToken()); 
-			}
-
-			Arrays.sort(container, Collections.reverseOrder());
-	
-			for (int i = 0; i < N; i++) {
-				container[0]--;
-				container[99]++;
-				Arrays.sort(container, Collections.reverseOrder());
-				
-			}
-			
-			System.out.println("#" + test_case + " " + (container[0] - container[99]));
-				
+		//int tc = Integer.parseInt(br.readLine());
+		
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("#" + i + " " + solution(br));
 		}
-
+		
 	}
 	
+	public static int solution(BufferedReader br) throws IOException {
+		int dump = Integer.parseInt(br.readLine());
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] container = new int[100];
+		
+		for (int i = 0; i < 100; i++) {
+			container[i] = Integer.parseInt(st.nextToken());
+		}
+		// 입력 종료 
 
+		for (int i = 0; i < dump; i++) {
+			Arrays.sort(container);
+			if (container[99] == container[0])
+				continue;
+
+			container[99]--;
+			container[0]++;
+		}
+		
+		Arrays.sort(container);
+		return container[99] - container[0];// 정답을 return 
+	}
 }
+
