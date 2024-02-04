7d04413d12130b1f5d7fac301bc4970a6c74f762
Unggu <130115689+unggu0704@users.noreply.github.com>
2024-02-04 15:52:27 +0900

    [Silver I] Title: 피아노 체조, Time: 1412 ms, Memory: 66396 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -16,7 +16,6 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
 
         int[] muscis = new int[N + 1];
-
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i < N + 1; i++) {
             muscis[i] = Integer.parseInt(st.nextToken());
@@ -25,7 +24,6 @@ public class Main {
         M = Integer.parseInt(br.readLine());
 
         int[][] questions = new int[M][2];
-        //입력 받을 때 배열의 인덱스에 맞추어 -1을 해준다.
         for (int i = 0; i < M ; i++) {
             st = new StringTokenizer(br.readLine());
             questions[i][0] = Integer.parseInt(st.nextToken());
@@ -34,15 +32,13 @@ public class Main {
         // 입력 종료
 
         int[] mistakeMusic = new int[N + 1];
-        int prefixSum = 0;
-
+        
         for (int i = 1; i < N; i++) {
             //뒤에 있는 악보보다 현재 어려운게 있다면
             if (muscis[i] >  muscis[i + 1]) {
                 mistakeMusic[i]++; // i 번재 악보에서는 반드시 틀린다.
             }
-            prefixSum += mistakeMusic[i];
-            mistakeMusic[i] = prefixSum;
+            mistakeMusic[i] += mistakeMusic[i -1];
         }
         //틀릴 악보 설정 -> 누적합 계산
 
@@ -51,14 +47,8 @@ public class Main {
             int end = questions[i][1] - 1;
 
             int mistakeCount = mistakeMusic[end] - mistakeMusic[start];
-
-            if (mistakeCount < 0 || start == end)
-                mistakeCount = 0;
-
             System.out.println(mistakeCount);
         }
-
-
     }
-
 }
+
