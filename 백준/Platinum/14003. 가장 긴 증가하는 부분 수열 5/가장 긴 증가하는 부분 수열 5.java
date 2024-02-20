aa3ffc001edfd756202ae19ab73c18f74fc16145
Unggu <130115689+unggu0704@users.noreply.github.com>
2024-02-20 10:34:46 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 976 ms, Memory: 185856 KB -BaekjoonHub

---
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * Main_16235_나무재테크_김규형
+ * 
+ * @author 김규형
+ */
+
+public class Main {
+
+	static int N;
+	static int M;
+	static int K;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int[] input = new int[N];
+		int[] binaryArr = new int[N];
+		int[] indexArr = new int[N];
+		int answer = 1;
+		
+ 		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < N; i++) {
+			input[i] = Integer.parseInt(st.nextToken());
+		}
+		// 입력 종료 
+		
+		Arrays.fill(binaryArr, Integer.MAX_VALUE);
+		
+		binaryArr[0] = input[0];
+		
+		for (int i = 0; i < N; i++) {
+			int index = Arrays.binarySearch(binaryArr, input[i]);
+			if (index < 0) { //배열에 이게 없다면 
+				index = (index + 1) * -1;
+				binaryArr[index] = input[i];
+				if (answer == index)
+					answer++;
+				
+			}
+			indexArr[i] = index;	// 해당 번호가 몇번째에 들어가야할지에 대한   index를 담고 있는 배열 
+		}
+		
+		
+		System.out.println(answer);
+		answer--;
+		
+		Stack<Integer> stack = new Stack<>();
+		for (int i = N - 1; i >= 0; i--) {
+			if (answer == - 1)
+				break;
+			
+			if (answer == indexArr[i]) {
+				stack.add(input[i]);
+				answer--;
+			}
+		}
+
+		int stackSize = stack.size();
+		for (int i = 0; i < stackSize; i++) {
+			if (i == 0)
+				sb.append(stack.pop());
+			else
+				sb.append(" ").append(stack.pop());
+		}
+		
+		System.out.print(sb.toString());
+		
+		
+	}
+	
+}
