d42b214c94c7ed4eb88b6eca20c4dd586c5eca17
Unggu <130115689+unggu0704@users.noreply.github.com>
2024-02-26 17:39:52 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,76 +1,59 @@
-import java.io.*;
-
-import java.util.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		solution();
-	}
-	static int ans = 0;
-    public static void solution() throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	StringTokenizer st;
-    	boolean flag = false;
-    	int N = Integer.parseInt(br.readLine());
-    	int Edge = Integer.parseInt(br.readLine());
-    	  	
-    	PC[] pcList = new PC[N];
-    	
-    	for(int i = 0; i< N; i++) {
-    		PC p = new PC(i+1);
-    		pcList[i] = p;
-    	}
-    	
-    	
-    	for(int i = 0; i < Edge; i++) {
-    		
+
+	static int N;
+	static int M;
+	static int[] parents;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		M = Integer.parseInt(br.readLine());
+		
+		parents = new int [N + 1];
+		for (int i = 1; i < N + 1; i++) {
+			parents[i] = i;
+		}
+		
+		StringTokenizer st;
+		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			
-			int input1 = Integer.parseInt(st.nextToken()) - 1;
-			int input2 = Integer.parseInt(st.nextToken()) - 1;
-			pcList[input1].linkList.add(pcList[input2]); // 연ruf 
-			pcList[input2].linkList.add(pcList[input1]); // 연ruf 
+			int input1 = Integer.parseInt(st.nextToken());
+			int input2 = Integer.parseInt(st.nextToken());
+			
+			if (find(input1) != find(input2)) {
+				union(input1, input2);
+			}
+		}
 		
-    	}
-    	// 입력 종료 
-    	
-    	Queue<PC> inflecList = new LinkedList<PC>();
-    	pcList[0].infelction = true;
-    	listCopy(pcList[0].linkList, inflecList);
-
-    	
-    	while(!inflecList.isEmpty()){
-    		PC pc = inflecList.poll();
-    	
-    		
-    		listCopy(pc.linkList, inflecList);
-    	}
-    	
-    	System.out.println(ans);
-    }
-    
-    static void listCopy(ArrayList<PC> a, Queue<PC> b) {
-    	for(PC i : a) {
-    		if(i.infelction) continue;
-    		ans++;
-    		i.infelction = true;
-    		b.add(i);
-    	}
-    }
-    
-}
-
-class PC{
-	ArrayList<PC> linkList = new ArrayList<>();
-	boolean infelction = false;
-	int name;
-	
-	PC(int n){
-		name = n;
+		int ans = 0;
+		for (int i : parents) {
+			if (find(i) == 1) 
+				ans++;
+		}
+		
+		System.out.println(ans - 1);
+		
 	}
 	
+	public static int find(int node) {
+		if (parents[node] == node) return node;
+		
+		return parents[node] = find(parents[node]);
+	}
 	
+	public static void union(int n1, int n2) {
+		int r1 = find(n1);
+		int r2 = find(n2);
+		
+		if (r1 > r2)
+			parents[r1] = r2;
+		else
+			parents[r2] = r1;
+	}
+
 }
-
-
