3a4b466598f1fdcad02bb947e2bcea864d54a0ba
Unggu <130115689+unggu0704@users.noreply.github.com>
2024-03-03 22:23:04 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 448 ms, Memory: 131000 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+coordinates = []
+for _ in range(N):
+    coordinates.append(list(map(int, input().split())))
+coordinates.sort(key=lambda x: (x[1], x[0]))
+for i in coordinates:
+    print (*i)
