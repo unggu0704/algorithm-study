6980e36282ef6c3515fa30d93e64859776f8c5c4
Kyuheong Kim <130115689+unggu0704@users.noreply.github.com>
2024-04-08 22:57:25 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	while(true) {
+		getline(cin, s);
+		if(s == "") 
+            break;
+
+		cout << s << '\n';
+	}
+	return 0;
+}
